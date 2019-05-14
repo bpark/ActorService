a183f2fe20b08a47fb48f7d6de991b752bd410c4
bpark <burt.parkers@gmail.com>
2019-05-14 19:47:44 +0200

    Quality and Balance are now ValueObjects

--- ActorService/Model/Actor.cs
+++ ActorService/Model/Actor.cs
@@ -1,25 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using CSharpFunctionalExtensions;
 
 namespace ActorService.Model
 {
-    
-    public class Quality {
-
+    public class Quality : ValueObject<Quality>
+    {
         public static readonly Quality Poor = new Quality(1.0f, "Poor");
         public static readonly Quality Common = new Quality(1.1f, "Common");
         public static readonly Quality Uncommon = new Quality(1.2f, "Uncommon");
         public static readonly Quality Rare = new Quality(1.3f, "Rare");
         public static readonly Quality Epic = new Quality(1.4f, "Epic");
         public static readonly Quality Legendary = new Quality(1.5f, "Legendary");
-            
-        public float Multiplier {get; }
+
+        public float Multiplier { get; }
         public string Name { get; }
 
-        public static IEnumerable<Quality> Values => new[] { Poor, Common, Uncommon, Rare, Epic, Legendary };
+        public static IReadOnlyList<Quality> Values => new[] {Poor, Common, Uncommon, Rare, Epic, Legendary};
 
-        private Quality(float multiplier, string name) {
+        private Quality(float multiplier, string name)
+        {
             Multiplier = multiplier;
             Name = name;
         }
@@ -33,13 +34,29 @@ namespace ActorService.Model
                     return value;
                 }
             }
+
             throw new ArgumentException("Invalid name: " + name);
         }
 
-    }
-    
-    public class Balance {
+        protected override bool EqualsCore(Quality other)
+        {
+            return base.Equals(other) && Multiplier.Equals(other.Multiplier) && string.Equals(Name, other.Name);
+        }
 
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                var hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ Multiplier.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Name?.GetHashCode() ?? 0);
+                return hashCode;
+            }
+        }
+    }
+
+    public class Balance : ValueObject<Balance>
+    {
         public static readonly Balance PP = new Balance(0, 20, 0, "PP");
         public static readonly Balance SS = new Balance(0, 0, 20, "SS");
         public static readonly Balance HH = new Balance(20, 0, 0, "HH");
@@ -50,14 +67,14 @@ namespace ActorService.Model
         public static readonly Balance SB = new Balance(4, 4, 9, "SB");
         public static readonly Balance HB = new Balance(9, 4, 4, "HB");
         public static readonly Balance BB = new Balance(5, 5, 5, "BB");
-            
+
         public float Health { get; }
         public float Power { get; }
         public float Speed { get; }
-        
+
         public string Name { get; }
-        
-        public static Balance[] Values => new[] { PP, SS, HH, HP, PS, HS, PB, SB, HB, BB };
+
+        public static Balance[] Values => new[] {PP, SS, HH, HP, PS, HS, PB, SB, HB, BB};
 
         private Balance(int health, int power, int speed, string name)
         {
@@ -66,7 +83,7 @@ namespace ActorService.Model
             Speed = speed;
             Name = name;
         }
-        
+
         public static Balance FromName(string name)
         {
             foreach (var value in Values)
@@ -76,17 +93,38 @@ namespace ActorService.Model
                     return value;
                 }
             }
+
             throw new ArgumentException("Invalid name: " + name);
         }
+
+        protected override bool EqualsCore(Balance other)
+        {
+            return base.Equals(other) && Health.Equals(other.Health) && Power.Equals(other.Power) &&
+                   Speed.Equals(other.Speed) && string.Equals(Name, other.Name);
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                var hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ Health.GetHashCode();
+                hashCode = (hashCode * 397) ^ Power.GetHashCode();
+                hashCode = (hashCode * 397) ^ Speed.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Name?.GetHashCode() ?? 0);
+                return hashCode;
+            }
+        }
+
     }
-    
+
     public class Actor
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Level { get; set; }
         public Quality Quality { get; set; }
-        
+
         public int CurrentHealth { get; set; }
 
         public int BaseHealth { get; set; }
@@ -96,17 +134,16 @@ namespace ActorService.Model
         public Balance Balance { get; set; }
 
         public int Experience { get; set; }
-        
+
         public IReadOnlyList<Ability> Abilities { get; set; }
 
         [NotMapped]
-        public int Health => (int)Math.Round((BaseHealth + Balance.Health / 10) * 5 * Level * Quality.Multiplier + 100);
+        public int Health =>
+            (int) Math.Round((BaseHealth + Balance.Health / 10) * 5 * Level * Quality.Multiplier + 100);
 
-        [NotMapped]
-        public int Power => (int) Math.Round((BasePower + Balance.Power / 10) * Level * Quality.Multiplier);
-        
-        [NotMapped]
-        public int Speed => (int) Math.Round((BaseSpeed + Balance.Speed / 10) * Level * Quality.Multiplier);
+        [NotMapped] public int Power => (int) Math.Round((BasePower + Balance.Power / 10) * Level * Quality.Multiplier);
+
+        [NotMapped] public int Speed => (int) Math.Round((BaseSpeed + Balance.Speed / 10) * Level * Quality.Multiplier);
 
         public override bool Equals(object obj)
         {
@@ -119,7 +156,7 @@ namespace ActorService.Model
         {
             return Id;
         }
-        
+
         private bool Equals(Actor other)
         {
             return Id == other.Id;
